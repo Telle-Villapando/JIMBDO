8fb8b3b73d5e44d8b810dc95560cec3ee50a34e4
ChristelleD_V <cd55villapando@gmail.com>
2024-08-15 04:00:51 +0800

    added files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'bottom-only': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)', // Customize values
+      },
+      lineHeight: {
+        '125': '1.25',  // 125% is equivalent to 1.25
+      },
+    },
+  },
+  plugins: [],
+}
+
