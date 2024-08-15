ffc1fc692fc335a63440b5c5df36893b050fab9f
ChristelleD_V <89006081+Telle-Villapando@users.noreply.github.com>
2024-08-15 14:51:26 +0800

    updated-contents

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,19 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        'bottom-only': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)', // Customize values
+        "bottom-only":
+          "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)", // Customize values
       },
       lineHeight: {
-        '125': '1.25',  // 125% is equivalent to 1.25
+        125: "1.25", // 125% is equivalent to 1.25
+      },
+      width: {
+        1520: "1520px",
+      },
+      margin: {
+        "28rem": "28rem",
       },
     },
   },
   plugins: [],
-}
-
+};
