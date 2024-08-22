8c35c01ebf7fb4a45761615e29640bfe6ef218c4
ChristelleD_V <89006081+Telle-Villapando@users.noreply.github.com>
2024-08-22 15:06:42 +0800

    AAHHH

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,11 @@ module.exports = {
         mb: "320px",
         md: "768px",
         dk: "1370px",
+        navDk: "993px",
         LL: "1440PX",
+        "max-991": { max: "991px" },
+        "min-768": { min: "768px" },
+        "min-992": { min: "992px" },
       },
       maxWidth: {
         1600: "1600px",
@@ -28,10 +32,13 @@ module.exports = {
       minWidth: {
         250: "250px",
       },
-      colors:{
+      colors: {
         darkBlue: "#001453",
         brightBlue: "#004ced",
-      }
+      },
+      boxShadow: {
+        "custom-shadow": "0 2px 12px 0 rgba(0, 0, 0, 0.06)",
+      },
     },
   },
   plugins: [],
